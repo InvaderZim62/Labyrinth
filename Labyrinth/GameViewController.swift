69da4eb848b5832b9dc547c0ad732fc2c55efb75
Invader Zim <pwstern@gmail.com>
2021-06-01 19:11:32 -0700

    Created panels around full labyrinth holes.

--- Labyrinth/GameViewController.swift
+++ Labyrinth/GameViewController.swift
@@ -45,8 +45,22 @@ class GameViewController: UIViewController {
     var boardNode: SCNNode!
     let motionManager = CMMotionManager()  // needed for accelerometers
     
-    let holeCentersX: [CGFloat] = [-1.815, -0.715, 1.155]
-    let holeCentersZ: [CGFloat] = [ 2.225, -2.105, 1.035]
+    // hole position relative to upper left corner of board
+    let holeCentersX: [CGFloat] = [
+        1.4, 10.4, 12.9, 14.3, 15.3, 9.1, 5.0, 2.5, 3.8, 13.1,
+        10.4, 5.0, 6.5, 7.7, 10.6, 14.3, 13.1, 3.8, 11.8, 5.1,
+        9.2, 1.3, 3.8, 15.1, 6.4, 5.1, 10.2, 2.4, 14.3, 13.1,
+        9.1, 1.2, 3.7, 10.4, 15.2, 7.8, 2.4, 5.1, 14.3, 10.4
+    ]
+    let holeCentersZ: [CGFloat] = [
+        1.7, 1.7, 1.7, 1.7, 2.4, 2.8, 3.4, 3.6, 3.6, 4.3,
+        4.4, 5.3, 5.6, 5.6, 5.9, 6.2, 6.7, 6.9, 7.1, 7.7,
+        7.8, 8.0, 9.1, 9.3, 10.1, 10.2, 10.2, 10.5, 10.8, 11.0,
+        11.1, 11.6, 11.9, 12.2, 12.3, 12.5, 12.7, 13.6, 13.6, 13.6
+    ]
+    
+    // 2D array of board locations with 0.1 resolution (origin at upper left corner)
+    var taken = Array(repeating: Array(repeating: false, count: Int(Constants.boardWidth * 10)), count: Int(Constants.boardHeight * 10))  // taken[z][x]
 
     override var shouldAutorotate: Bool {
         return true
@@ -109,7 +123,7 @@ class GameViewController: UIViewController {
 
         // cover board with kinematic panels (except for holes)
         createBoardPanels()
-        createBoardBumpers()
+        createBoardEdges()
 
         let marble = SCNSphere(radius: Constants.marbleRadius)
         marble.firstMaterial?.diffuse.contents = UIColor.lightGray
@@ -137,35 +151,85 @@ class GameViewController: UIViewController {
         super.viewWillDisappear(animated)
         motionManager.stopAccelerometerUpdates()
     }
-    
+
     private func createBoardPanels() {
-        var rightOfPriorHole = -Constants.boardWidth / 2
+        // start by marking square areas covering holes as taken
         for index in holeCentersX.indices {
-            // left of hole
-            createBoardPanel(x1: rightOfPriorHole,
-                             x2: holeCentersX[index] - Constants.holeRadius,
-                             z1: -Constants.boardWidth / 2,
-                             z2: Constants.boardWidth / 2)
-            // above hole
-            createBoardPanel(x1: holeCentersX[index] - Constants.holeRadius,
-                             x2: holeCentersX[index] + Constants.holeRadius,
-                             z1: -Constants.boardWidth / 2,
-                             z2: holeCentersZ[index] - Constants.holeRadius)
-            // below hole
-            createBoardPanel(x1: holeCentersX[index] - Constants.holeRadius,
-                             x2: holeCentersX[index] + Constants.holeRadius,
-                             z1: holeCentersZ[index] + Constants.holeRadius,
-                             z2: Constants.boardWidth / 2)
-            rightOfPriorHole = holeCentersX[index] + Constants.holeRadius
+//            createHolePanelAt(centerX: holeCentersX[index] - Constants.boardWidth / 2, centerZ: holeCentersZ[index] - Constants.boardHeight / 2)
+            updateTakenArrayForHoleAt(centerX: holeCentersX[index], centerZ: holeCentersZ[index])
         }
-        // right of last hole
-        createBoardPanel(x1: rightOfPriorHole,
-                         x2: Constants.boardWidth / 2,
-                         z1: -Constants.boardWidth / 2,
-                         z2: Constants.boardWidth / 2)
+        // fit panels from upper left corner of open space, to right until encountering an obstacle,
+        // then down until encountering an obstacle, until all space is taken
+        while true {
+            if let upperLeftCorner = nextOpening() {
+                let lowerRightCorner = findLowerRightCornerStartingFrom(upperLeftCorner)
+                updateTakenArrayForPanelFrom(upperLeftCorner, to: lowerRightCorner)
+                createBoardPanelAt(x1: CGFloat(upperLeftCorner.x) / 10 - Constants.boardWidth / 2,  // convert from taken coords to board coords
+                                   x2: CGFloat(lowerRightCorner.x) / 10 - Constants.boardWidth / 2,
+                                   z1: CGFloat(upperLeftCorner.z) / 10 - Constants.boardHeight / 2,
+                                   z2: CGFloat(lowerRightCorner.z) / 10 - Constants.boardHeight / 2)
+            } else {
+                break
+            }
+        }
+    }
+    
+    // move from left to right, top to bottom, to find first open location
+    private func nextOpening() -> (z: Int, x: Int)? {
+        for z in 0..<taken.count {
+            for x in 0..<taken[0].count {
+                if !taken[z][x] { return (z, x) }
+            }
+        }
+        return nil
     }
 
-    private func createBoardPanel(x1: CGFloat, x2: CGFloat, z1: CGFloat, z2: CGFloat) {
+    private func findLowerRightCornerStartingFrom(_ upperLeftCorner: (z: Int, x: Int)) -> (z: Int, x: Int) {  // origin at upper left corner of board
+        var rightSide = taken[0].count
+        let bottom = taken.count
+        for z in upperLeftCorner.z..<bottom {
+            for x in upperLeftCorner.x..<rightSide {
+                if taken[z][x] {
+                    if z == upperLeftCorner.z {
+                        rightSide = x  // encountering obstacle in first row determines right side of opening
+                    } else {
+                        return (z, rightSide)  // encountering obstacle after first row determines bottom of opening
+                    }
+                    break
+                }
+            }
+        }
+        return (bottom, rightSide)
+    }
+    
+    private func updateTakenArrayForHoleAt(centerX: CGFloat, centerZ: CGFloat) {  // origin at upper left corner of board
+        for x in Int((centerX - Constants.holeRadius) * 10)..<Int((centerX + Constants.holeRadius) * 10) {
+            for z in Int((centerZ - Constants.holeRadius) * 10)..<Int((centerZ + Constants.holeRadius) * 10) {
+                taken[z][x] = true
+            }
+        }
+    }
+    
+    private func updateTakenArrayForPanelFrom(_ upperLeftCorner: (z: Int, x: Int), to lowerRightCorner: (z: Int, x: Int)) {
+        for x in upperLeftCorner.x..<lowerRightCorner.x {
+            for z in upperLeftCorner.z..<lowerRightCorner.z {
+                taken[z][x] = true
+            }
+        }
+    }
+
+    // this was used during development/debugging
+    private func createHolePanelAt(centerX: CGFloat, centerZ: CGFloat) {  // origin at center of board
+        let panel = SCNBox(width: 2 * Constants.holeRadius, height: Constants.boardThickness, length: 2 * Constants.holeRadius, chamferRadius: 0)
+        panel.firstMaterial?.diffuse.contents = UIColor(displayP3Red: 0.8, green: 0.8, blue: 0.8, alpha: 0.5)
+        let panelNode = SCNNode(geometry: panel)
+        panelNode.name = "hole"
+        panelNode.position = SCNVector3(x: Float(centerX), y: 0, z: Float(centerZ))
+        panelNode.physicsBody = SCNPhysicsBody(type: .kinematic, shape: nil)
+        boardNode.addChildNode(panelNode)
+    }
+
+    private func createBoardPanelAt(x1: CGFloat, x2: CGFloat, z1: CGFloat, z2: CGFloat) {  // origin at center of board
         let panel = SCNBox(width: x2 - x1, height: Constants.boardThickness, length: z2 - z1, chamferRadius: 0)
         panel.firstMaterial?.diffuse.contents = Constants.panelColor
         let panelNode = SCNNode(geometry: panel)
@@ -174,16 +238,16 @@ class GameViewController: UIViewController {
         boardNode.addChildNode(panelNode)
     }
     
-    private func createBoardBumpers() {
+    private func createBoardEdges() {
         let halfBoardWidth = Constants.boardWidth / 2
         let halfBoardHeight = Constants.boardHeight / 2
-        createBoardBumper(x1: -halfBoardWidth, x2: halfBoardWidth, z1: -halfBoardHeight, z2: -halfBoardHeight + Constants.bumperWidth)  // top
-        createBoardBumper(x1: -halfBoardWidth, x2: halfBoardWidth, z1: halfBoardHeight - Constants.bumperWidth, z2: halfBoardHeight)  // bottom
-        createBoardBumper(x1: -halfBoardWidth, x2: -halfBoardWidth + Constants.bumperWidth, z1: -halfBoardHeight + Constants.bumperWidth, z2: halfBoardHeight - Constants.bumperWidth)  // left
-        createBoardBumper(x1: halfBoardWidth - Constants.bumperWidth, x2: halfBoardWidth, z1: -halfBoardHeight + Constants.bumperWidth, z2: halfBoardHeight - Constants.bumperWidth)  // right
+        createBoardEdge(x1: -halfBoardWidth, x2: halfBoardWidth, z1: -halfBoardHeight, z2: -halfBoardHeight + Constants.bumperWidth)  // top
+        createBoardEdge(x1: -halfBoardWidth, x2: halfBoardWidth, z1: halfBoardHeight - Constants.bumperWidth, z2: halfBoardHeight)  // bottom
+        createBoardEdge(x1: -halfBoardWidth, x2: -halfBoardWidth + Constants.bumperWidth, z1: -halfBoardHeight + Constants.bumperWidth, z2: halfBoardHeight - Constants.bumperWidth)  // left
+        createBoardEdge(x1: halfBoardWidth - Constants.bumperWidth, x2: halfBoardWidth, z1: -halfBoardHeight + Constants.bumperWidth, z2: halfBoardHeight - Constants.bumperWidth)  // right
     }
     
-    private func createBoardBumper(x1: CGFloat, x2: CGFloat, z1: CGFloat, z2: CGFloat) {
+    private func createBoardEdge(x1: CGFloat, x2: CGFloat, z1: CGFloat, z2: CGFloat) {
         let bumper = SCNBox(width: x2 - x1, height: Constants.bumperThickness, length: z2 - z1, chamferRadius: 0)
         bumper.firstMaterial?.diffuse.contents = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
         let bumperNode = SCNNode(geometry: bumper)
